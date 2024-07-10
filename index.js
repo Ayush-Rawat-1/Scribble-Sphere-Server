cf2c25cc2c162f295936144e326afe3c645186c0
Ayush Rawat <149077649+Ayush-Rawat-1@users.noreply.github.com>
2024-07-10 11:31:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(passport.initialize());
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Visit https://scribblesphere0.vercel.app/"));
 
 app.use("/api/auth",authRoute);
 
